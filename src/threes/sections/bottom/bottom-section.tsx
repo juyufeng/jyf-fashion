804bef3d34117b468f6ff356d32fc1e48e6428f2
juyufeng <345108530@qq.com>
2025-04-20 14:14:35 +0800

    feat: 添加react-icons依赖并更新底部和右侧部分样式
    添加react-icons依赖以支持图标功能，同时更新底部和右侧部分的样式，提升UI视觉效果

--- src/threes/sections/bottom/bottom-section.tsx
+++ src/threes/sections/bottom/bottom-section.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const BottomSection = () => {
-  return <div>下</div>;
+  return (
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      backgroundColor: '#c0c0c0', // 经典的灰色背景
+      borderTop: '1px solid #a0a0a0', // 边框
+      boxShadow: '0 -1px 5px rgba(0, 0, 0, 0.2)', // 阴影
+      bottom: 0,
+      width: '100%',
+      height: '100%',
+    }}>
+      <div>
+        1111
+      </div>
+      <div>
+        2222
+      </div>
+    </div>
+  );
 };
 
 export default BottomSection;
--- src/threes/sections/right/right-section.tsx
+++ src/threes/sections/right/right-section.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const RightSection = () => {
-  return <div>右</div>;
+  return <div style={{width: '100%', height: '100%', background: 'rgb(245,245,245)'}}>
+    
+  </div>;
 };
 
 export default RightSection;
